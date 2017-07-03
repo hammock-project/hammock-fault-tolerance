a36f96ee16bb7f133ab78b44ebe6bcb73b2da9d9
John D. Ament <johndament@apache.org>
2017-07-03 07:19:58 -0400

    Minor extension clean up, cache the annotations instead of reading each time.  Clean up how fallbacks are checked.

--- src/main/java/ws/ament/hammock/ft/extension/FaultToleranceExtension.java
+++ src/main/java/ws/ament/hammock/ft/extension/FaultToleranceExtension.java
@@ -1,35 +1,59 @@
 package ws.ament.hammock.ft.extension;
 
 
-import org.eclipse.microprofile.faulttolerance.Asynchronous;
-import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
-import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.*;
 import ws.ament.hammock.ft.interceptor.FaultTolerant;
 
 import javax.enterprise.event.Observes;
 import javax.enterprise.inject.spi.*;
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.lang.reflect.Type;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
 
 import static ws.ament.hammock.ft.interceptor.FaultTolerant.INSTANCE;
 
 public class FaultToleranceExtension implements Extension {
+
     public void findFaultTolerantBeans(@Observes @WithAnnotations({Retry.class, CircuitBreaker.class, Asynchronous.class})
-                                       ProcessAnnotatedType<?> pat) {
-        if(!pat.getAnnotatedType().isAnnotationPresent(FaultTolerant.class)) {
+                                               ProcessAnnotatedType<?> pat) {
+        if (!pat.getAnnotatedType().isAnnotationPresent(FaultTolerant.class)) {
             pat.setAnnotatedType(new FaultTolerantAnnotatedTypeWrapper(pat.getAnnotatedType()));
         }
     }
 
+    private final List<Exception> failures = new ArrayList<>();
+
+    public void verifyFallBacks(@Observes @WithAnnotations({Fallback.class}) ProcessAnnotatedType<?> pat) throws Exception {
+        pat.getAnnotatedType().getMethods().stream().filter(m -> m.isAnnotationPresent(Fallback.class)).forEach(m -> {
+            Fallback fallback = m.getAnnotation(Fallback.class);
+            try {
+                Method handle = fallback.value().getMethod("handle", ExecutionContext.class);
+                if (!handle.getReturnType().equals(m.getJavaMember().getReturnType())) {
+                    failures.add(new IllegalStateException("The method " + handle + " returns a different type than " + m.getJavaMember()));
+                }
+            } catch (NoSuchMethodException ignore) {
+
+            }
+        });
+    }
+
+    public void afterDeployValidation(@Observes AfterDeploymentValidation afterDeploymentValidation) {
+        failures.forEach(afterDeploymentValidation::addDeploymentProblem);
+    }
 
     private static class FaultTolerantAnnotatedTypeWrapper<X> implements AnnotatedType<X> {
 
         private final AnnotatedType<X> delegate;
+        private final Set<Annotation> annotations;
 
         private FaultTolerantAnnotatedTypeWrapper(AnnotatedType<X> delegate) {
             this.delegate = delegate;
+            Set<Annotation> annotations = delegate.getAnnotations();
+            Set<Annotation> allAnotations = new LinkedHashSet<>();
+            allAnotations.add(INSTANCE);
+            allAnotations.addAll(annotations);
+            this.annotations = allAnotations;
         }
 
         @Override
@@ -64,21 +88,17 @@ public class FaultToleranceExtension implements Extension {
 
         @Override
         public <T extends Annotation> T getAnnotation(Class<T> annotationType) {
-            return annotationType == FaultTolerant.class ? (T) INSTANCE : delegate.getAnnotation(annotationType);
+            return FaultTolerant.class.equals(annotationType) ? (T) INSTANCE : delegate.getAnnotation(annotationType);
         }
 
         @Override
         public Set<Annotation> getAnnotations() {
-            Set<Annotation> annotations = delegate.getAnnotations();
-            Set<Annotation> allAnotations = new LinkedHashSet<>();
-            allAnotations.add(INSTANCE);
-            allAnotations.addAll(annotations);
-            return allAnotations;
+            return Collections.unmodifiableSet(annotations);
         }
 
         @Override
         public boolean isAnnotationPresent(Class<? extends Annotation> annotationType) {
-            return annotationType == FaultTolerant.class || delegate.isAnnotationPresent(annotationType);
+            return FaultTolerant.class.equals(annotationType) || delegate.isAnnotationPresent(annotationType);
         }
     }
 }
